9e3f87550e3593cfe221c3f21ade0825d5c5df7e
JoaoHLira <lirarosas.jh@gmail.com>
2023-07-24 11:31:41 -0300

    inicia projeto

---
+++ src/main/java/br/com/joaolira/lembreremedio/LembreRemedioApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaolira.lembreremedio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembreRemedioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembreRemedioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaolira/lembreremedio/LembreRemedioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaolira.lembreremedio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembreRemedioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
