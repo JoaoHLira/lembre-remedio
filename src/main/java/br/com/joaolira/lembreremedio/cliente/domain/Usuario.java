23691fb85e18ce63f7b3e5e687e453831c673372
JoaoHLira <lirarosas.jh@gmail.com>
2023-07-24 12:07:02 -0300

    implementa dominio de negocio

---
+++ src/main/java/br/com/joaolira/lembreremedio/cliente/domain/Usuario.java
@@ -0,0 +1,26 @@
+package br.com.joaolira.lembreremedio.cliente.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Getter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private UUID idUsuario;
+    @NotBlank
+    private String nomeCompleto;
+    @Email
+    @Column(unique = true)
+    private String email;
+}
