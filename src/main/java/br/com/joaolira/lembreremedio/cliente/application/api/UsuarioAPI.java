411f7bd99294999287c951a5acd8a2b7e128458e
JoaoHLira <lirarosas.jh@gmail.com>
2023-07-25 10:17:21 -0300

    implementa contrato da API

---
+++ src/main/java/br/com/joaolira/lembreremedio/cliente/application/api/UsuarioAPI.java
@@ -0,0 +1,14 @@
+package br.com.joaolira.lembreremedio.cliente.application.api;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/usuario")
+public interface UsuarioAPI {
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
+}
